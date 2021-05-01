6db43a035e82b8bd7c79f63d5242f445e8c9856f
hopin1005 <asd89541321@gmail.com>
2021-05-01 01:21:31 -0700

    user input default template

---
+++ action_template.js
@@ -0,0 +1,19 @@
+var action  = {
+    "type": "box",
+    "layout": "baseline",
+    "contents": [
+      {
+        "type": "icon",
+        "url": "https://image.flaticon.com/icons/png/512/197/197335.png"
+      },
+      {
+        "type": "text",
+        "text": "吃飯",
+        "weight": "bold",
+        "margin": "sm",
+        "flex": 0
+      }
+    ]
+}
+
+exports.action = action;
