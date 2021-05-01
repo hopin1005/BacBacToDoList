e7f630c17b633e1707a12be41101992f13959b86
hopin1005 <asd89541321@gmail.com>
2021-05-01 01:20:51 -0700

    add update function in to do list

--- app.js
+++ app.js
@@ -16,12 +16,15 @@ const mysql = require("mysql");
 var bacbactemplate = require('./bacbactemplate');
 bacbactemplate = bacbactemplate['bacbactemplate'];
 
+
 //empty template
 var empty_bacbactemplate = require('./empty_bacbactemplate');
 empty_template = empty_bacbactemplate['empty_bacbactemplate'];
 
-//full expect json data
-const util = require('util');
+//action_template
+var action_template = require('./action_template.js');
+action_template = action_template['action'];
+
 
 // Line Channel info
 const bot = linebot({
@@ -44,9 +47,9 @@ app.listen(3000);
 
 //when user follow
 bot.on('follow', function(event){
-
+	
 	event.reply([
-  		"歡迎來到BacBacToDoList! 按下方按鈕開始吧! 還能上傳圖片置換上面的白囉!",empty_template]);
+  		"歡迎來到BacBacToDoList! 按下方按鈕開始吧! 還能上傳圖片置換上面!",empty_template]);
 
 });
 
@@ -62,48 +65,111 @@ bot.on('follow', function(event){
 bot.on('postback', function(event){
 
 	var data = event.postback.data
+	var userid = event.source.userId;
 	switch (data){
 		case 'teachadd':
-			//create user data
-			var userid = event.source.userId;
-			
-			//insert data if user not exist
-			//maybe sql injection?? 
-			tmp_template = util.inspect(bacbactemplate, {showHidden: false, depth: null})
-			sql_query = 'insert into linebot (id, template) select * from (select ' + '"' + userid + '",' + '"' + tmp_template  + '"' + ') as tmp where not exists(select id from linebot where id = '+ '"' + userid + '"'  + ');';
 
+			//create user data
+			//insert data if user not exist
+			//maybe sql injection??
+			var tmp_template = JSON.stringify(bacbactemplate);
+			
+			sql_query = "insert into linebot (id, template) select * from (select " + "'" + userid + "',"  + "'" + tmp_template + "'" + ") as tmp where not exists(select id from linebot where id = "+ "'" + userid + "'"  + ");";
 
 			query(sql_query, function(err, results){
 
                 		if (err) {
                         		console.log('Encountered an error:', err.message);
-                        		return res.send(500, err.message);
                 		}
         		});
+			
+			//get user_template from database
+                        sql_query = mysql.format("select template from linebot where id = ?;", userid);
+                        query(sql_query, function(err, results){
+                                if (err) {
+                                        console.log("Encountered an error:", err.message);
+                                }
+					
+				
+				//reply user template
+				var res = JSON.parse(results[0].template);
+				
+				event.reply(["直接輸入想記錄的就可以囉!" , res])
+                        });
 
-
-			event.reply(["直接輸入你想記錄的事情就可以囉!", bacbactemplate])
+			
 			break;
 		
 		case 'show':
-			event.reply(["你現在的ToDoList：" , bacbactemplate])
+			
+			//get user_template from database
+			sql_query = mysql.format("select template from linebot where id = ?;", [userid]);
+			query(sql_query, function(err, results){
+				if (err) {
+					console.log("Encountered an error:", err.message);	
+				}
+				
+				//reply user template
+				var res = JSON.parse(results[0].template);
+				event.reply(["你現在的ToDoList：" , res])
+			});
 			break;
+		
 
 		case 'alter':
 			break;
 
-		//default is add to ToDoList
+		
 		default:
-			event.reply("ddd");
+			return;
 	}
 	
 })
 
 
-//user input message
-//1. add to list
+//user input message will be added to list
 bot.on('message', function(event){
 	
-	event.reply(event.source.userId);
+	var userid = event.source.userId;
+	
+	//add user any  input to template
+	var input = event.message.text;
+
+	//input data to action_template
+	action_template.contents[1].text = input;
+
+
+	//insert action_template to database;
+	//get user_template from database;
+	sql_query = mysql.format("select template from linebot where id = ?;", [userid]);
+        query(sql_query, function(err, results){
+        	if (err) {
+                	console.log("Encountered an error:", err.message);
+                }
+
+                //insert action_template to user_template;
+		var res = JSON.parse(results[0].template);
+		res.contents.body.contents[1].contents.push(action_template)
+			
+		event.reply(["你現在的ToDoList：" , res]);
+		
+		//update database 
+		update(res, userid);
+        });
+	
+	//update database
+	function update(data, userid){
+		
+		var temp_template = JSON.stringify(data);
+		
+		var sql_query = "update linebot set template = '"+ temp_template  + "' where id = '" + userid + "';";
+		query(sql_query, function(err, results){
+                	if (err) {
+                        	console.log("Encountered an error:", err.message);
+                	}
+			
+		});
+
+	}
 
 })
