9b4ebd974c3c4cee813adf492ececa16222255fe
hopin1005 <asd89541321@gmail.com>
2021-05-01 07:31:14 -0700

    add del function

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const query = require('./sql_conn');
 const mysql = require("mysql");
 
 
-
 //import bacbac template
 //full template
 var bacbactemplate = require('./bacbactemplate');
@@ -25,6 +24,12 @@ empty_template = empty_bacbactemplate['empty_bacbactemplate'];
 var action_template = require('./action_template.js');
 action_template = action_template['action'];
 
+//del template
+var del_template = require('./del_template.js');
+del_template = del_template['deltemplate'];
+
+var push_template = require('./del_template.js');
+push_template = push_template['pushtemplate'];
 
 // Line Channel info
 const bot = linebot({
@@ -65,16 +70,21 @@ bot.on('follow', function(event){
 bot.on('postback', function(event){
 
 	var data = event.postback.data
-	var userid = event.source.userId;
-	switch (data){
-		case 'teachadd':
+	
 
-			//create user data
-			//insert data if user not exist
+	var userid = event.source.userId;
+	switch (true){
+		case /^teachadd/.test(data):
+
+			//create user data -> create user_template and deltemplate
+			//insert data if user not exist 
 			//maybe sql injection??
-			var tmp_template = JSON.stringify(bacbactemplate);
 			
-			sql_query = "insert into linebot (id, template) select * from (select " + "'" + userid + "',"  + "'" + tmp_template + "'" + ") as tmp where not exists(select id from linebot where id = "+ "'" + userid + "'"  + ");";
+			var tmp_template = JSON.stringify(bacbactemplate);
+			var deltmp_template = JSON.stringify(del_template);
+
+			sql_query = "insert into linebot (id, template, deltemplate) select * from (select " + "'" + userid + "',"  + "'" + tmp_template + "'," + "'" + deltmp_template  +"'" + ") as tmp where not exists(select id from linebot where id = "+ "'" + userid + "'"  + ");";
+		
 
 			query(sql_query, function(err, results){
 
@@ -82,6 +92,7 @@ bot.on('postback', function(event){
                         		console.log('Encountered an error:', err.message);
                 		}
         		});
+
 			
 			//get user_template from database
                         sql_query = mysql.format("select template from linebot where id = ?;", userid);
@@ -92,6 +103,7 @@ bot.on('postback', function(event){
 					
 				
 				//reply user template
+				
 				var res = JSON.parse(results[0].template);
 				
 				event.reply(["直接輸入想記錄的就可以囉!" , res])
@@ -100,7 +112,7 @@ bot.on('postback', function(event){
 			
 			break;
 		
-		case 'show':
+		case /^show/.test(data):
 			
 			//get user_template from database
 			sql_query = mysql.format("select template from linebot where id = ?;", [userid]);
@@ -114,13 +126,57 @@ bot.on('postback', function(event){
 				event.reply(["你現在的ToDoList：" , res])
 			});
 			break;
-		
 
-		case 'alter':
+		//teach delte and show deltemplate
+		case /^del/.test(data):
+			
+			sql_query = mysql.format("select deltemplate from linebot where id = ?;", [userid]);
+
+                        query(sql_query, function(err, results){
+                                if (err) {
+                                        console.log("Encountered an error:", err.message);
+                                }
+
+                                var res = JSON.parse(results[0].deltemplate);
+				event.reply(res);
+
+			});
+
+			
 			break;
 
-		
+
+		case /^action=del/.test(data):
+			
+			//del user data;
+			//slice delete item
+			var del_item = data.split("=")[2];
+			
+
+			//delete item from database;
+			//get deltemplate from database
+			sql_query = mysql.format("select deltemplate from linebot where id = ?;", [userid]);
+
+			query(sql_query, function(err, results){
+                        	if (err) {
+                                	console.log("Encountered an error:", err.message);
+                        	}
+
+				var res = JSON.parse(results[0].deltemplate);
+
+				//find item and delete
+				console.log(res.template.columns[0].actions);
+
+                	});
+
+
+			//
+			break;
+
+			
 		default:
+			//do nothing;
+			event.reply("Ohhhhh, you may do wrong!");
 			return;
 	}
 	
@@ -134,35 +190,42 @@ bot.on('message', function(event){
 	
 	//add user any  input to template
 	var input = event.message.text;
-
-	//input data to action_template
+	
+	//input data to action_template and del_template
 	action_template.contents[1].text = input;
-
+	push_template.data = `action=del&itemid=${input}`;
+	push_template.label = input;
 
 	//insert action_template to database;
 	//get user_template from database;
-	sql_query = mysql.format("select template from linebot where id = ?;", [userid]);
+	sql_query = mysql.format("select template, deltemplate from linebot where id = ?;", [userid]);
         query(sql_query, function(err, results){
         	if (err) {
                 	console.log("Encountered an error:", err.message);
                 }
 
                 //insert action_template to user_template;
+		
 		var res = JSON.parse(results[0].template);
 		res.contents.body.contents[1].contents.push(action_template)
-			
+		
+		var delres = JSON.parse(results[0].deltemplate);
+		delres.template.columns[0].actions.push(push_template);
+		
 		event.reply(["你現在的ToDoList：" , res]);
 		
 		//update database 
-		update(res, userid);
+		update(res, delres, userid);
         });
 	
 	//update database
-	function update(data, userid){
+	function update(data, delres, userid){
 		
 		var temp_template = JSON.stringify(data);
+		var deltemp_template = JSON.stringify(delres);
+
+		var sql_query = "update linebot set template = '"+ temp_template  + "' ,deltemplate = '" + deltemp_template + "' where id = '" + userid + "';";
 		
-		var sql_query = "update linebot set template = '"+ temp_template  + "' where id = '" + userid + "';";
 		query(sql_query, function(err, results){
                 	if (err) {
                         	console.log("Encountered an error:", err.message);
@@ -173,3 +236,8 @@ bot.on('message', function(event){
 	}
 
 })
+
+//todo 
+//add id in database;
+//add 0~2 -> new block
+//id -> index -> del;
