ed19e3c828bd7cf4522c24486215ef6551856282
hopin1005 <asd89541321@gmail.com>
2021-05-02 03:15:57 -0700

    merge template file

--- bacbactemplate.js
+++ bacbactemplate.js
@@ -71,4 +71,24 @@ var bacbactemplate = {
 }
 
 }
+
+var action  = {
+    "type": "box",
+    "layout": "baseline",
+    "contents": [
+      {
+        "type": "icon",
+        "url": "https://image.flaticon.com/icons/png/512/197/197335.png"
+      },
+      {
+        "type": "text",
+        "text": "吃飯",
+        "weight": "bold",
+        "margin": "sm",
+        "flex": 0
+      }
+    ]
+}
+
+exports.action = action;
 exports.bacbactemplate = bacbactemplate;
