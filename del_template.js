705483efc96244ce1e65302d1c894d6a12c66d7c
hopin1005 <asd89541321@gmail.com>
2021-05-02 03:16:26 -0700

    update del template

--- del_template.js
+++ del_template.js
@@ -1,24 +1,80 @@
 var deltemplate = {
-  "type": "template",
-  "altText": "Delete item",
-  "template": {
-      "type": "carousel",
-      "columns": [
+  "type" : "flex",
+  "altText" : "empty_bac",
+  "contents":{
+  "type": "carousel",
+  "contents": [
+	  //new block insert 
+    {
+      "type": "bubble",
+      "size": "micro",
+      "hero": {
+        "type": "image",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ93rKZdHAerhqktsVJc2BMAwE0Y8NF2zx1OSKmrsriB1yJbNob8vW9oNoPUCVcqyJTOj0&usqp=CAU",
+        "size": "full",
+        "aspectMode": "cover",
+        "aspectRatio": "320:213"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
           {
-            "title": "點擊你想刪除的!",
-            "text": "勇敢點下去!",
-            "actions": [
-            ]
-          }
-      ],
-  }
+            "type": "text",
+            "weight": "bold",
+            "size": "sm",
+            "wrap": true,
+            "text": "你要刪除什麼?"
+          }//single block insert
+        ],
+        "spacing": "sm",
+        "paddingAll": "13px"
+      }
+    }
+  ]
+}
 }
 
+
 var pushtemplate = {
-  "type": "postback",
-  "label": "del",
-  "data": "action=del&itemid="
+  "type": "button",
+  "action": {
+  	"type": "postback",
+  	"label": "del",
+  	"data": "action=del&itemid=",
+  	"displayText": "delete to do list"
+  },
+  "style": "primary",
+  "color": "#905c44"
 }
+
+var newcarousel = {
+      "type": "bubble",
+      "size": "micro",
+      "hero": {
+        "type": "image",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ93rKZdHAerhqktsVJc2BMAwE0Y8NF2zx1OSKmrsriB1yJbNob8vW9oNoPUCVcqyJTOj0&usqp=CAU",
+        "size": "full",
+        "aspectMode": "cover",
+        "aspectRatio": "320:213"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "weight": "bold",
+            "size": "sm",
+            "wrap": true,
+            "text": "你要刪除什麼?"
+          }
+        ],
+        "spacing": "sm",
+        "paddingAll": "13px"
+      }
+    }
 
 exports.deltemplate = deltemplate;
 exports.pushtemplate = pushtemplate;
+exports.newcarousel = newcarousel;
